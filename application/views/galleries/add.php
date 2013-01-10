d4bf3f91f166713197646c16cb79afcfb66c5957
Tomasz Bula <tbula@future-processing.com>
2013-01-10 09:46:34 +0100

    Fixed upload files

--- application/views/galleries/add.php
+++ application/views/galleries/add.php
@@ -1,8 +1,8 @@
 <div class="offset 2 span5">
     <form action="Galleries/add" method="post">
         <p>
-            Title:<input type="text" name="galleryName"></input>
-            <input type="submit" value="Submit"></input>
+            Title:<input type="text" name="galleryName">
+            <input type="submit" value="Submit">
         </p>
     </form>
 </div>
@@ -13,10 +13,7 @@
 <div class="span8">
     <?php if (isset($images)): ?>
         <?php foreach ($images as $image) : ?>
+        <img src="<?php echo $image['path'] ?>" width="100" height="100"></img>
         <?php endforeach; ?>
     <?php endif; ?>
-
-
-
-    }
 </div>
